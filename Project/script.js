6209408f580887bbf37cf841cd5abba43ce6ce63
RidaAbid7 <ridaabid767@gmail.com>
2024-03-20 17:04:40 +0500

    changes in landing page

--- Project/script.js
+++ Project/script.js
@@ -1,8 +1,12 @@
 function doBindings() {
     let submitBtn = document.getElementById("submitBtn");
     submitBtn.onclick = handleFormSubmission; 
+    scrolldown;
 }
 $(doBindings);
+// $(document).ready(function(){
+//     scrolldown();
+// });
 
 function handleFormSubmission(event) {
     var name = $('#name').val();
@@ -13,4 +17,21 @@ function handleFormSubmission(event) {
         alert('Please fill in all fields.');
         event.preventDefault();
     }
+}
+  
+  
+
+
+
+
+function scrolldown(){
+    document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+        anchor.addEventListener('click', function (e) {
+            e.preventDefault();
+
+            document.querySelector(this.getAttribute('href')).scrollIntoView({
+                behavior: 'smooth'
+            });
+        });
+    });
 }
