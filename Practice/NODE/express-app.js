313beb07f608a919c5ec09fde9777655059c3f39
RidaAbid7 <ridaabid767@gmail.com>
2024-05-03 00:32:48 +0500

    project updated

--- Practice/NODE/express-app.js
+++ Practice/NODE/express-app.js
@@ -2,24 +2,38 @@
 //run it from same folder in which you are building your server
 // npm i -g nodemon
 //run above command once in lifetime.
+
 const express = require("express");
+const mongoose = require("mongoose");
 let server = express();
-server.use(express.static("public"));
+let Student = require("./models/Students");
+server.use(express.json());
 server.set("view engine", "ejs");
-//above line sets ejs as view engine
-server.get("/api/stories", function (req, res) {
-  res.send([
-    { title: "Story 1", content: "story 1 content" },
-    { title: "story 2", content: "story 2 content" },
-  ]);
-});
-server.get("/contact-us", function (req, res) {
-  //route to handle / get request
+
+let ejsLayouts = require("express-ejs-layouts");
+server.use(ejsLayouts);
+
+server.use(express.static("public"));
+
+let studentsAPIRouter = require("./routes/api/students");
+server.use(studentsAPIRouter);
+
+server.get("/contact-us", async (req, res) => {
   res.render("contact-us");
 });
-server.get("/", function (req, res) {
-  //route to handle / get request
+
+server.get("/", async (req, res) => {
   res.render("homepage");
 });
 
-server.listen(4000);
+server.listen(4000, () => {
+  console.log("server started listening at localhost:4000");
+});
+mongoose
+  .connect("mongodb+srv://ridaabid7:Ridaabid267@project1.tbva6z8.mongodb.net/")
+  .then(() => {
+    console.log("DB Connected");
+  })
+  .catch((err) => {
+    console.log("Unable to connect");
+  });
